7c3960a398207473c979e28d85ac4c9e5f0b9d1c
Priambudi Lintang Bagaskara <priambudibagaskara@gmail.com>
2023-01-21 13:21:24 +0800

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [counter2, setCounter2] = useState(0)
+
+  // const text = 'Hello this is me'
+  // const num = 12345
+  const [text, num] = ['Hello this is me', 12345]
+
+  // Ubah data array of string ini menjadi array of JSX Element
+  const arr = ["Afif Fasya", "Yoko", "2H4B6D"]
+  const arrObj = [{
+    name: "John",
+    job: "Gamer"
+  },
+  {
+    name: "Cena",
+    job: "Bing Chilling"
+  }]
+
+  useEffect(() => {
+    console.log("dijalanin 1x aja")
+  }, [])
+
+  useEffect(() => {
+    console.log("I detect changes in state 'Counter' / 'Counter2' now: " + counter + '/' + counter2)
+  }, [counter, counter2])
+
+  const handleClickAdd = () => {
+    if (counter === 10) {
+      setCounter(0)
+      return;
+    }
+    setCounter(prev => prev + 1)
+    return;
+  }
+
+  const handleClickAdd2 = () => {
+    if (counter2 === 10) {
+      setCounter2(0)
+      return;
+    }
+    setCounter2(prev => prev + 1)
+    return;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Counter: {counter}
+      <button onClick={handleClickAdd}>Tambah</button>
+      Counter2: {counter2}
+      <button onClick={handleClickAdd2}>Tambah 2</button>
+      <br />
+      <p>{text}</p>
+      {num}
+      {arr.map((data, index) => {
+        return <p key={index}>{data + index}</p>
+      })}
+      {arrObj.map((data, index) => {
+        return <p key={index}>{data.name} - {data.job}</p>
+      })}
+    </>
   );
 }
 
